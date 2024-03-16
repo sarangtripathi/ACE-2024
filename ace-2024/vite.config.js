3f4d827c9e8653c7b7c7586e384da23aae3a9fb9
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2024-03-16 12:49:46 +0530

    Folder Structure

--- ace-2024/vite.config.js
+++ ace-2024/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // server: {
+  //   proxy: {
+  //     "/api/v1": "http://localhost:9090/",
+  //   },
+  // },
+
   plugins: [react()],
-})
+});
