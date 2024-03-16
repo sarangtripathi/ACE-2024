73ca0cb3b6c53243b67ea98a718a7fa7fb25c1e8
Sarang Tripathi <sarangtripathi18@gmail.com>
2024-03-16 12:08:54 +0530

    1

---
+++ ace-2024/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Index1 from './components/Index1'
+
+
+function App() {
+  
+
+  return (
+    <>
+     < Index1 />
+    </>
+  )
+}
+
+export default App
---
+++ ace-2024/src/components/Index1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index1 = () => {
+  return (
+    <div className=''>Index1</div>
+  )
+}
+
+export default Index1
