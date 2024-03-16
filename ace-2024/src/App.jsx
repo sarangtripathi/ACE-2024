f0b9e99c4a591b2b5ccbdad11bcf6a931a331f62
Sarang Tripathi <sarangtripathi18@gmail.com>
2024-03-16 12:35:58 +0530

    routes

--- ace-2024/src/App.jsx
+++ ace-2024/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Index1 from './components/Index1'
+import Login from './components/Login/Login'
+import Home from './components/Home/Home';
+import Signup from './components/Signup/Signup';
 
 
 function App() {
-  
+  const alert = useAlert();
 
   return (
-    <>
-     < Index1 />
-    </>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/index1" element={<Index1/>}  />
+      </Routes>
+    </Router>
   )
 }
 
